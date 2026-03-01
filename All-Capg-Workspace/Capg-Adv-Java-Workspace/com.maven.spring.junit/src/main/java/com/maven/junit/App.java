1c2dae44404ecc85141e8b5ac1f68247f558a76f
YOSKERnerv <sagarpakar777@gmail.com>
2026-03-02 04:23:47 +0530

    added

---
+++ All-Capg-Workspace/Capg-Adv-Java-Workspace/com.maven.spring.junit/src/main/java/com/maven/junit/App.java
@@ -0,0 +1,13 @@
+package com.maven.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
